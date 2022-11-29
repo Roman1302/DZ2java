d00493dd33f4b998c02ff74339af0a4b3ade063e
Роман Бухмистов <rrb13@mail.ru>
2022-11-30 00:11:25 +0300

    решена 2 задача

---
+++ dz2_1.java
@@ -0,0 +1,23 @@
+// Дана строка sql-запроса "select * from students where ". 
+// Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+// Данные для фильтрации приведены ниже в виде json строки. 
+// Если значение null, то параметр не должен попадать в запрос. 
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+public class dz2_1 {
+
+    public static void main(String[] args) {
+        StringBuilder targetStr = new StringBuilder();
+        String[] jsStr = { "name:Ivanov", "country:Russia", "city:Moscow", "age:null" };
+        int len = jsStr.length;
+        for (int i = 0; i < len; i++) {
+            if (jsStr[i].indexOf("null") == (-1)) {
+                targetStr.append(jsStr[i].replace(":", "="));
+                if (i != len - 2) {
+                    targetStr.append(" AND ");
+                }
+            }
+        }
+        System.out.println("часть Where запроса: " + targetStr);
+    }
+}
